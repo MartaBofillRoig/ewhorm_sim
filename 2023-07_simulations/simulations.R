d41cbb9fd1b45e77ecfd38e3f31d969aaf6e676d
Marta Bofill Roig <marta.bofillroig@meduniwien.ac.at>
2023-07-27 14:23:52 +0200

    aux

--- 2023-07_simulations/simulations.R
+++ 2023-07_simulations/simulations.R
@@ -12,28 +12,11 @@ set.seed(123)
 mu=c(0,1,2,5)
 N1 = 30*4
 
-# # Example
-# db = sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=0.1)
-# model_6m = lm(y_6m~treat,data=db)
-# model_12m = lm(y_12m~treat,data=db)
-# summary(model_6m)
-# summary(model_12m)
-
-# Load the multcomp package
-# library(multcomp)
-# 
-# g<-glht(mmm(model_6m,model_12m),
-#      mlf(mcp(treat = "Dunnett")), alternative = "greater")
-
-# var_cov_matrix <- vcov(g)
-
 # Function to simulate trial data (2-stages, with dose selection)
 sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha1=0.5){
   
   # stage1
   db_stage1 = sim_data(n_arms=n_arms, N=N1, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=sd_y)
-  
-  
   res_stage1 = DunnettTest(x=db_stage1$y_6m, g=db_stage1$treat) 
   
   # selection
@@ -62,21 +45,24 @@ sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha
   sset_hyp3 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[3,][hyp[3,] != 0])])
   sset_hyp4 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[4,][hyp[4,] != 0])])
   
-  # model_12m = lm(y_12m~treat,data=sset_hyp1)
-  # mm=summary(model_12m)
-  # mm$coefficients
-  # result_anova1[[1]][[5]][[1]]
+  # pvalues closed test
   
   pvalue_anova1 <- summary(aov(y_12m ~ treat, data = sset_hyp1))[[1]][[5]][[1]]
   pvalue_anova2 <- summary(aov(y_12m ~ treat, data = sset_hyp2))[[1]][[5]][[1]]
   pvalue_anova3 <- summary(aov(y_12m ~ treat, data = sset_hyp3))[[1]][[5]][[1]]
   pvalue_anova4 <- summary(aov(y_12m ~ treat, data = sset_hyp4))[[1]][[5]][[1]]
   
+  pvalue_stage1 <- XX
+  
   # stage2
   db_stage2 = sim_data(n_arms=2, N=N2, mu_6m=mu[c(1,sel)], mu_12m=mu[c(1,sel)]+c(0,1,1,2)[c(1,sel)], sd_y=sd_y)
   levels(db_stage2$treat) = levels(db_stage1$treat)[c(1,sel)]
+  pvalue_stage2 <- summary(aov(y_12m ~ treat, data = db_stage2))[[1]][[5]][[1]]
   
-  list_res=list(db_stage1,db_stage2)
+  # Inverse normal combination test
+  combined_pvalue = 1 - pnorm(qnorm(1 - pvalue_stage1) / sqrt(2) + qnorm(1 - pvalue_stage2) / sqrt(2))
+  
+  list_res=list(db_stage1,db_stage2,sel,combined_pvalue)
   
   return(list_res)
 }
@@ -86,5 +72,11 @@ sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha
 res = sim_trial(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1)
 res
 
+# # Example
+# db = sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=0.1)
+# model_6m = lm(y_6m~treat,data=db)
+# model_12m = lm(y_12m~treat,data=db)
+# summary(model_6m)
+# summary(model_12m) 
 
 
