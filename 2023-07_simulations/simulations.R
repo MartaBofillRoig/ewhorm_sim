b74b5f45e70095263d1c6b5a4ed255d15e28aa75
Marta Bofill Roig <marta.bofillroig@meduniwien.ac.at>
2023-07-27 13:05:31 +0200

    closedtest

--- 2023-07_simulations/simulations.R
+++ 2023-07_simulations/simulations.R
@@ -4,66 +4,28 @@
 # Marta Bofill Roig
 ##########################
 
+setwd("C:/Users/mbofi/Dropbox/CeMSIIS/GitHub/ewhorm_sim/2023-07_simulations")
+source("aux.R")
+
 # Settings
 set.seed(123)
 mu=c(0,1,2,5)
 N1 = 30*4
 
-# install.packages("DescTools")
-library(DescTools)
+# # Example
+# db = sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=0.1)
+# model_6m = lm(y_6m~treat,data=db)
+# model_12m = lm(y_12m~treat,data=db)
+# summary(model_6m)
+# summary(model_12m)
 
-# Prelude - Auxiliary functions
-# Function to get the column index of the maximum value in a row
-get_max_col_index <- function(row) {
-  return(which.max(row))
-}
+# Load the multcomp package
+# library(multcomp)
+# 
+# g<-glht(mmm(model_6m,model_12m),
+#      mlf(mcp(treat = "Dunnett")), alternative = "greater")
 
-# Function to compute the hypotheses to test (closed test)
-get_hyp_mat <- function(n_hypothesis = 3, selected_hypothesis = 1){
-  elements <- c(rep(0, n_hypothesis), 1:n_hypothesis)
-  
-  hyp_mat <- unique(combinations(n=length(elements), r=n_hypothesis, v=elements, set = F, repeats.allowed = F))
-  
-  selected_rows <- hyp_mat[which(apply(hyp_mat == selected_hypothesis, 1, any)), ]
-  
-  selected_rows
-}
-# get_hyp_mat(3,2)
-
-
-
-# Function to simulate trial data (1-stage, multiple arms)
-sim_data <- function(n_arms, N, mu_6m, mu_12m, sd_y=0.1){ 
-
-  treatments <- factor(sample(1:n_arms, N, replace = TRUE), 
-                       levels = 1:n_arms, 
-                       labels = c("Placebo", "Low", "Medium", "High")[1:n_arms])
-  X <- model.matrix(~ treatments - 1)
-  
-  # simulate trial data
-  y_6m = X %*% mu_6m + rnorm(N, mean = 0, sd = sd_y)
-  y_12m = X %*% mu_12m + rnorm(N, mean = 0, sd = sd_y)
-  
-  # Treatment indicator for dataframe 
-  max_col_indices <- apply(X, 1, get_max_col_index)
-  treat=unname(max_col_indices)
-  
-  treat=factor(treat, levels = 1:n_arms,
-               labels = c("Placebo", "Low", "Medium", "High")[1:n_arms]) 
-  
-  # Output
-  data=data.frame(y_6m=y_6m, y_12m=y_12m, treat=treat)
-
-  return(data)
-}
-
-# Example
-db = sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=0.1)
-model_6m = lm(y_6m~treat,data=db)
-model_12 = lm(y_12m~treat,data=db)
-summary(model_6m)
-summary(model_12)
-
+# var_cov_matrix <- vcov(g)
 
 # Function to simulate trial data (2-stages, with dose selection)
 sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha1=0.5){
@@ -75,30 +37,40 @@ sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha
   res_stage1 = DunnettTest(x=db_stage1$y_6m, g=db_stage1$treat) 
   
   # selection
-  
-  #Worst case: No trend is seen in any of the doses (e.g., all p> alpha1): select highest dose
+  #--- Worst case: No trend is seen in any of the doses (e.g., all p> alpha1): select highest dose
   if(sum(res_stage1$Placebo[,4]>alpha1)==3){ 
     sel=4
   }
-  #Intermediate case: some doses show a trend: select the (highest) dose, no new recruitment for the other doses
+  #--- Intermediate case: some doses show a trend: select the (highest) dose, no new recruitment for the other doses
   if(sum(res_stage1$Placebo[,4]<alpha1)<3){
     sel=which.min(res_stage1$Placebo[,4])+1
   }
-  #Intermediate case 2: some doses show a trend: select the lowest effective dose, no new recruitment for the other doses
+  #--- Intermediate case 2: some doses show a trend: select the lowest effective dose, no new recruitment for the other doses
   if(sum(res_stage1$Placebo[,4]<alpha1)<3){
     sel=which.min(res_stage1$Placebo[,4])+1
   }
-  # Best case
+  #--- Best case
   if(sum(res_stage1$Placebo[,4]<alpha1)==3){
     sel=2
   }
   
-  db_hyp= subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,2,4)])
-  DunnettTest(x=db_hyp$y_6m, g=db_hyp$treat) 
+  hyp <- get_hyp_mat(3,sel) 
+  hyp <- hyp + (hyp != 0)
   
-  t.test(db_stage1)
-  hyp_closedtest(sel=3)
+  sset_hyp1 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[1,][hyp[1,] != 0])])
+  sset_hyp2 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[2,][hyp[2,] != 0])])
+  sset_hyp3 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[3,][hyp[3,] != 0])])
+  sset_hyp4 <- subset(db_stage1,db_stage1$treat==levels(db_stage1$treat)[c(1,hyp[4,][hyp[4,] != 0])])
   
+  # model_12m = lm(y_12m~treat,data=sset_hyp1)
+  # mm=summary(model_12m)
+  # mm$coefficients
+  # result_anova1[[1]][[5]][[1]]
+  
+  pvalue_anova1 <- summary(aov(y_12m ~ treat, data = sset_hyp1))[[1]][[5]][[1]]
+  pvalue_anova2 <- summary(aov(y_12m ~ treat, data = sset_hyp2))[[1]][[5]][[1]]
+  pvalue_anova3 <- summary(aov(y_12m ~ treat, data = sset_hyp3))[[1]][[5]][[1]]
+  pvalue_anova4 <- summary(aov(y_12m ~ treat, data = sset_hyp4))[[1]][[5]][[1]]
   
   # stage2
   db_stage2 = sim_data(n_arms=2, N=N2, mu_6m=mu[c(1,sel)], mu_12m=mu[c(1,sel)]+c(0,1,1,2)[c(1,sel)], sd_y=sd_y)
@@ -113,15 +85,6 @@ sim_trial <- function(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1, alpha
 # n_arms=4; N1=30*4; N2=30*2; mu=c(0,1,2,5); sd_y=0.1
 res = sim_trial(n_arms=4, N1=30*4, N2=30*2, mu=c(0,1,2,5), sd_y=0.1)
 res
-
-# Function to decide the selected dose
-select_dose <- function(){
-  
-}
-
-select_dose_borrow <- function(){
-  
-}
 
 
 
