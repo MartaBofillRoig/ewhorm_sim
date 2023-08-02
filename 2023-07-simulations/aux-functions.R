32884da2c0e9f6cd89ca429d705e656588c908b7
Marta Bofill Roig <marta.bofillroig@meduniwien.ac.at>
2023-08-02 12:14:17 +0200

    update sim_data
    simulating two correlated continuous endpoints

--- 2023-07-simulations/aux-functions.R
+++ 2023-07-simulations/aux-functions.R
@@ -28,33 +28,3 @@ get_hyp_mat <- function(n_hypothesis = 3, selected_hypothesis = 1){
 # Example
 # get_hyp_mat(3,2)
 
-# Function to simulate trial data (1-stage, multiple arms)
-sim_data <- function(n_arms, N, mu_6m, mu_12m, sd_y=0.1){ 
-  
-  treatments <- factor(sample(1:n_arms, N, replace = TRUE), 
-                       levels = 1:n_arms, 
-                       labels = c("Placebo", "Low", "Medium", "High")[1:n_arms])
-  X <- model.matrix(~ treatments - 1)
-  
-  # simulate trial data
-  y_6m = X %*% mu_6m + rnorm(N, mean = 0, sd = sd_y)
-  y_12m = X %*% mu_12m + rnorm(N, mean = 0, sd = sd_y)
-  
-  # Treatment indicator for dataframe 
-  max_col_indices <- apply(X, 1, get_max_col_index)
-  treat=unname(max_col_indices)
-  
-  treat=factor(treat, levels = 1:n_arms,
-               labels = c("Placebo", "Low", "Medium", "High")[1:n_arms]) 
-  
-  # Output
-  data=data.frame(y_6m=y_6m, y_12m=y_12m, treat=treat)
-  
-  return(data)
-}
-# Example
-# db = sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sd_y=0.1) 
-# model_6m = lm(y_6m~treat,data=db)
-# model_12m = lm(y_12m~treat,data=db)
-# summary(model_6m)
-# summary(model_12m) 
---
+++ 2023-07-simulations/sim_data.R
@@ -0,0 +1,35 @@
+#' Simulate data from a multi-arm trial with shared control 
+#' @description Function to simulate trial data (1-stage, multiple arms)
+#' 
+#' @param n_arms number of arms (including control)
+#' @param N total sample size
+#' @param mu_6m 6-month mean response per arm (vector of length `n_arm`)
+#' @param mu_12m 12-month mean response per arm (vector of length `n_arm`)
+#' @param sigma covariance matrix between 6- and 12-month responses assumed equal across arms (matrix of dim 2x2)
+#' @returns simulated data
+#' @example 
+#' mu=c(0,0,0,0); sigma=matrix(c(0.1,0,0,0.1), nrow = 2, byrow = T)
+#' y=sim_data(n_arms=4, N=30*4, mu_6m=mu, mu_12m=mu+c(0,1,1,2), sigma=sigma) 
+#' @details eWHORM simulations
+#' @author Marta Bofill Roig
+
+sim_data <- function(n_arms, N, mu_6m, mu_12m, sigma){ 
+  
+  treatments <- factor(sample(1:n_arms, N, replace = TRUE), 
+                       levels = 1:n_arms, 
+                       labels = c("Placebo", "Low", "Medium", "High")[1:n_arms])
+  X <- model.matrix(~ treatments - 1) 
+  y <- X %*% matrix(c(mu_6m, mu_12m), nrow=4, byrow = F) + rmvnorm(n=N, mean = c(0,0), sigma = sigma ) 
+  
+  # Treatment indicator for dataframe 
+  max_col_indices <- apply(X, 1, get_max_col_index)
+  treat=unname(max_col_indices)
+  
+  treat=factor(treat, levels = 1:n_arms,
+               labels = c("Placebo", "Low", "Medium", "High")[1:n_arms]) 
+  
+  # Output
+  data=data.frame(y_6m=y[,1], y_12m=y[,2], treat=treat)
+  
+  return(data)
+}
