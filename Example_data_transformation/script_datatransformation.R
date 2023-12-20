84ba48b550799e629d0f8666ff71032190e8d3f5
mougeni <fabricemougeni@gmail.com>
2023-12-20 15:31:35 +0100

    script updated
    Using vector and apply to simplify the script

--- Example_data_transformation/script_datatransformation.R
+++ Example_data_transformation/script_datatransformation.R
@@ -1,6 +1,4 @@
-\subsubsection{Code}
 
-\begin{lstlisting}[language=R]
 # Importing packages
 ####################
 
@@ -22,7 +20,7 @@ mn <- 650; sd <- 575
 ###############
 
 r0 <- 0.15; r1 <- 0.6; r2 <- 0.8; r3 <- 0.9;
-
+reduct_rate <- c(r0, r1, r2, r3)
 
 #Correlation between the difference 
 ###################################
@@ -33,10 +31,11 @@ rho <- 0.5
 #The mean after six months from the common baseline mn
 ######################################################
 
-mn0 <- (1 - r0)*mn
-mn1 <- (1 - r1)*mn
-mn2 <- (1 - r2)*mn
-mn3 <- (1 - r3)*mn
+dtmn <- as.data.frame(reduct_rate)
+
+mean_6m <- data.frame(apply(dtmn, 2, function(r){ (1 - r)*mn}))
+names(mean_6m) <- "mean_6month"
+
 
 sd0 <- sd #we assumed the same sd for all the group at the baseline only
 
@@ -47,10 +46,15 @@ sde0 <- sqrt(log(1+(sd0^2/mn^2) )) # sd(log(x0))  Baseline
 #As we assumed that the estimated sigma for the log is the same for every other, 
 #we can deduce the sdi for each of the other group, to estimate a good mean for the log
 
-sd06m <- mn0*sqrt(exp(sde0) - 1) # sd((x6m)) 
-sd16m <- mn1*sqrt(exp(sde0) - 1) # sd((x6m)) LOW D. for twelve months
-sd26m <- mn2*sqrt(exp(sde0) - 1) # sd((x6m)) Medium D. for twelve months
-sd36m <- mn3*sqrt(exp(sde0) - 1) # sd((x6m)) HIGH for twelve months
+# sd06m <- mean_vec[1]*sqrt(exp(sde0) - 1) # sd((x6m)) 
+# sd16m <- mean_vec[2]*sqrt(exp(sde0) - 1) # sd((x6m)) LOW D. for twelve months
+# sd26m <- mean_vec[3]*sqrt(exp(sde0) - 1) # sd((x6m)) Medium D. for twelve months
+# sd36m <- mean_vec[4]*sqrt(exp(sde0) - 1) # sd((x6m)) HIGH for twelve months
+
+
+
+sd_6m <- data.frame(apply(dtmn, 2, function(m){ m*sqrt(exp(sde0) - 1)}))
+names(sd_6m) <- "sd_6month"
 
 
 #Twelve month : THIS IS THE SAME CODE TO SIX MONTH SO I CAN SKIP THE CODE AT THIS POINT UNLESS WE HAVE NEW VALUES FOR THE REDUCTION RATE AND SD
@@ -62,41 +66,46 @@ sd36m <- mn3*sqrt(exp(sde0) - 1) # sd((x6m)) HIGH for twelve months
 #Start with mean of original scale
 ##################################
 
-mn0 <- (1 - r0)*mn
-mn1 <- (1 - r1)*mn
-mn2 <- (1 - r2)*mn
-mn3 <- (1 - r3)*mn
-
-
-sd0 <- sd #we assumed the same sd for all the group at the baseline only
-
-moy0 <- log(mn^2/sqrt(mn^2 + sd0^2)) # mean(log(x0))  BASELINE
-sde0 <- sqrt(log(1+(sd0^2/mn^2) )) # sd(log(x0))  Baseline
-
+mean_12m <- mean_6m
+names(mean_12m) <- "mean_12month"
 #Standard deviation of the log(X)
 #################################
 
-sd012m <- mn0*sqrt(exp(sde0) - 1) # sd((x12m)) 
-sd112m <- mn1*sqrt(exp(sde0) - 1) # sd((x12m)) LOW D. for twelve months
-sd212m <- mn2*sqrt(exp(sde0) - 1) # sd((x12m)) Medium D. for twelve months
-sd312m <- mn3*sqrt(exp(sde0) - 1) # sd((x12m)) HIGH for twelve months
-
+sd_12m <- sd_6m
 
 ##############################################
 # PART 2 : Setting the all for the simulation
 ##############################################
 
-#Let's start with the mean for the log
+#Let's start with the mean for the log: Combine mean and sd in the same data.fr
 ######################################
 
-moy1 <- log(mn0^2/sqrt(mn0^2 + sd012m^2)) # mean(log(x0)) PLACEBO for twelve months
-moy2 <- log(mn1^2/sqrt(mn1^2+sd112m^2)) # mean(log(x6m)) LOW D. for twelve months
-moy3 <-  log(mn2^2/sqrt(mn2^2+sd212m^2))# mean(log(x6m)) Medium D. for twelve months
-moy4 <-  log(mn3^2/sqrt(mn3^2+sd312m^2))# mean(log(x6m)) HIGH for twelve months
 
-mu0 <-  c(moy0, moy0, moy0, moy0)
-mu6 <- c(moy1, moy2, moy3, moy4)
-mu12 <- c(moy1, moy2, moy3, moy4)
+mean_sd_6m <- cbind(mn = mean_6m, sd = sd_6m)
+
+#Please write a function to calculate directly the mean log
+###########################################################
+
+mslg <- function(mean_6month, sd_6month){
+  
+  moy <- log(mean_6month^2/sqrt(mean_6month^2 + sd_6month^2))
+  
+  return(moy)
+}
+
+#Now apply it
+#############
+
+mulog <- apply(mean_sd_6m, 1, function(m){ 
+  do.call(mslg, as.list(m))
+  })
+
+#Obtain the vector for each time point
+######################################
+
+mu0 <-  rep(moy0, 4) # since at the baseline they are the same
+mu6 <- mulog # for each arm
+mu12 <- mulog
 
 
 
@@ -128,4 +137,3 @@ cov_X12m_X12m <- var12m  #cov(log(X12m), log(X12m))
 sg <- matrix(c(varb, cov_X0_X6m, cov_X0_X12m, cov_X6_X0, var6m, cov_X6_X12m,
                cov_X12m_X0, cov_X12m_X6, cov_X12m_X12m), ncol=3)
 
-\end{lstlisting}
